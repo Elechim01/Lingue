2debd43a42a28fbbbed2c2c4413ea3f9e0a677ca
Elechim01 <mikymanny015@gmail.com>
2021-02-16 16:32:59 +0100

    Initial Commit

---
+++ Lingue/LingueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingueApp.swift
+//  Lingue
+//
+//  Created by Michele Manniello on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LingueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
